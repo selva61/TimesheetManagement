5a32fb78f378105a9956ea4e9d54b77c0adc11da
Gowthamraj2399 <gowthamraj2399@gmail.com>
2021-06-15 00:10:53 +0530

    bootstrap added, sidebar outline, login out line

--- TimesheetManagementUI/src/app/app.module.ts
+++ TimesheetManagementUI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screens/login/login.component';
+import { UsersComponent } from './screens/users/users.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
