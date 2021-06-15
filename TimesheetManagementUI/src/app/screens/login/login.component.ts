ad6be3026c6f613b89b22bf41302b2bf1b11636c
Gowthamraj2399 <gowthamraj2399@gmail.com>
2021-06-15 19:46:12 +0530

    Design corrections, added form validations.

--- TimesheetManagementUI/src/app/screens/login/login.component.ts
+++ TimesheetManagementUI/src/app/screens/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onLoginSubmit(loginForm: NgForm) {
+    console.log(loginForm);
   }
-
 }
